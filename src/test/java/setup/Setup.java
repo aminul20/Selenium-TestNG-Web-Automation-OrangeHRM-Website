be3635e2853784cc37e4f511f47e9d4175758b7b
Aminul Islam <aminul.cse14@gmail.com>
2023-09-30 00:02:52 +0600

    dependency package added & First run complete 'login'

---
+++ src/test/java/setup/Setup.java
@@ -0,0 +1,27 @@
+package setup;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.time.Duration;
+
+public class Setup {
+    public WebDriver driver;
+    @BeforeTest
+    public void setup(){
+        WebDriverManager.firefoxdriver().setup();
+        driver=new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+    }
+    @AfterTest
+    public void closeDriver(){
+        // DashboardPage dashboardPage=new DashboardPage(driver);
+        // dashboardPage.doLogout();
+//        driver.close();
+    }
+}
